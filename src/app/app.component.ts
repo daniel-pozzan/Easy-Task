a9889e2088e27dfea6e0c821c954d9e8339f0365
DESKTOP-HH0N8T9\daniel <daniel.pozzan@hotmail.com>
2024-06-05 08:14:49 +0200

    Add task and user components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'easy-task';
+  users = DUMMY_USERS;
+  selectedUserId: string = "u1";
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!.name
+  }
+
+  onSelectUser(id: string){
+    this.selectedUserId = id; 
+  } 
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+// type User = {
+//   id: string,
+//   avatar: string,
+//   name: string
+// }
+
+interface User {
+  id: string,
+  avatar: string,
+  name: string
+}
 
 @Component({
   selector: 'app-user',
@@ -8,5 +20,15 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  @Input({required: true}) user!: User;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
 
 }
